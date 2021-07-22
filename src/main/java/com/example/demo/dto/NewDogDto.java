47c9f790b89b6d671b9da713f55d65dd4422e8bd
Анастасия Шадрина <anastasia.shadrina@datamatica.ru>
2021-07-22 17:08:20 +0700

    Added DTO

---
+++ src/main/java/com/example/demo/dto/NewDogDto.java
@@ -0,0 +1,36 @@
+package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+public class NewDogDto {
+
+    private String name;
+
+    private Integer age;
+
+    private LocalDateTime birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDateTime getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDateTime birthday) {
+        this.birthday = birthday;
+    }
+}
